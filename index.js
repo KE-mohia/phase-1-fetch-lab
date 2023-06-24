b493fc15e2fc7667937345b3b977c1e75564208b
wayne <keyawayne1@gmail.com>
2023-06-24 09:51:36 +0300

    Assinment done

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  // Add a fetch request to the Game of Therones API
-  return fetch("https://anapioficeandfire.com/api/books")
-  .then((response) => response.json()) //convert the response to JSON
-  .then((json) => rendetBook(json)); // call rendorBooks() with the JSON data
-}
+ 
+ 
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  function fetchBooks() {
+    
+    // To pass the tests, don't forget to return your fetch!
+    // Add a fetch request to the Game of Therones API
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json()) //convert the response to JSON
+    .then((json) => renderBooks(json)); // call rendorBooks() with the JSON data
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
